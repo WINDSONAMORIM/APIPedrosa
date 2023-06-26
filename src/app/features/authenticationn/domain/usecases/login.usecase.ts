37ba09ce312c1b16d648a93a667a197c34a291ab
windsonamorim <windson.amorim@gmail.com>
2023-06-25 22:02:43 -0300

    refactor: add tryCathc no usecase Login

--- src/app/features/authenticationn/domain/usecases/login.usecase.ts
+++ src/app/features/authenticationn/domain/usecases/login.usecase.ts
@@ -1,31 +1,37 @@
-import { BCryptPassword } from '../../../../shared/adapters/crypto';
-import { JwtToken } from '../../../../shared/adapters/jwt';
-import { CustomError } from '../../../../shared/errors';
-import { UserSharedRepository } from '../../../../shared/infra/repositories/user-shared.repository';
-import { LoginDTO, LoginDetailDTO } from '../dtos';
+import { BCryptPassword } from "../../../../shared/adapters/crypto";
+import { JwtToken } from "../../../../shared/adapters/jwt";
+import { CustomError } from "../../../../shared/errors";
+import { UserSharedRepository } from "../../../../shared/infra/repositories/user-shared.repository";
+import { LoginDTO, LoginDetailDTO } from "../dtos";
 
 export class LoginUseCase {
-    async execute(loginDTO: LoginDTO): Promise<LoginDetailDTO> {
-        const repository = new UserSharedRepository();
+  async execute(loginDTO: LoginDTO): Promise<LoginDetailDTO> {
+    try {
+      const repository = new UserSharedRepository();
 
-        const user = await repository.getUserByEmail(loginDTO.email);
+      const user = await repository.getUserByEmail(loginDTO.email);
 
-        if (!user) throw new CustomError('Incorret email or password.');
+      if (!user) throw new CustomError("Incorret email or password.");
 
-        if(user.password != loginDTO.password) throw new CustomError("Incorret email or password.");
+      if (user.password != loginDTO.password)
+        throw new CustomError("Incorret email or password.");
 
-        const jwtToken = new JwtToken();
+      const jwtToken = new JwtToken();
 
-        const userData = {
-            id: user.id,
-            name: user.name,
-            email: user.email,
-            profile: user.profile,
-        };
+      const userData = {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        profile: user.profile,
+      };
 
-        const token = jwtToken.sign(userData);
-        return {
-            ...userData,token
-        };
+      const token = jwtToken.sign(userData);
+      return {
+        ...userData,
+        token,
+      };
+    } catch (error: any) {
+      throw error;
     }
+  }
 }
